c99d0f22298767e5facd0175f3431004084cbcdc
miawithcode <cuttontail@gmail.com>
2024-01-06 14:47:50 +0800

    feat: form structure

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,22 @@
+import { useState } from 'react';
+
 const Form = () => {
+  const [color, setColor] = useState('');
+  const handleSubmit = (e) => {};
+
   return (
-    <div>Form</div>
-  )
-}
-export default Form
+    <section>
+      <h4>Color Generator</h4>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+          placeholder="#f15025"
+        />
+        <button type='submit' style={{background: color}}>Generate</button>
+      </form>
+    </section>
+  );
+};
+export default Form;
