6b825f9b13b8659d1b025405d1b135d79e267406
miawithcode <cuttontail@gmail.com>
2024-01-06 17:29:15 +0800

    feat: copy color to clipboard

--- src/components/SingleColor.jsx
+++ src/components/SingleColor.jsx
@@ -1,10 +1,29 @@
-const SingleColor = ({color, index}) => {
-  const {hex, weight} = color;
+import { toast } from "react-toastify";
+
+const SingleColor = ({ color, index }) => {
+  const { hex, weight } = color;
+  const saveToClipboard = async () => {
+    if(navigator.clipboard) {
+      try {
+        await navigator.clipboard.writeText(`#${hex}`);
+        toast.success('Color copied to clipboard');
+      } catch (error) {
+        toast.error('Failed copy color to clipboard');
+      }
+    } else {
+      toast.error('Clipboard access not available')
+    }
+  }
+
   return (
-    <article className={index > 10 ? 'color color-light' : 'color'} style={{ background: `#${hex}` }}>
+    <article
+      className={index > 10 ? 'color color-light' : 'color'}
+      style={{ background: `#${hex}` }}
+      onClick={saveToClipboard}
+    >
       <p>#{hex}</p>
       <p>{weight}%</p>
     </article>
   );
-}
+};
 export default SingleColor
